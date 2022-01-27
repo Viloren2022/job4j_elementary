b830256b05a473ae9ae3e6c1ef500ccabf80db07
Viloren88 <garifullin.marat@gmail.com>
2022-01-27 21:52:34 +0300

    6. Идеальный вес [#41584]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -9,7 +9,7 @@ public class Fit {
 
     public static double womanWeight(short height) {
         double rsl = (height - 110) * 1.15;
-        return rsl;
+        return rsl; //test
     }
 
     public static void main(String[] args) {
