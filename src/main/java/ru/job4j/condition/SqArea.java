3c1941e6515c3f79d8c7d54fb38fdcd12f62b823
Viloren88 <garifullin.marat@gmail.com>
2022-01-27 23:46:52 +0300

    9. Площадь треугольника. [#175474]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(double p, double k) {
-
         double height = p / 2 * (k + 1);
         double length = height * k;
         double rsl = height * length;
