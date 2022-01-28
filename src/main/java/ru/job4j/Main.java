fd8d6768bf60facff97855ca4e357bc9468d692c
Viloren88 <garifullin.marat@gmail.com>
2022-01-28 23:07:43 +0300

    3. Арифметические операции. [#185 #256338]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Marat");
         long l = 129;
         byte b = (byte) l;
         System.out.println(b);
