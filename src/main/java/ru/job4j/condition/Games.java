cfa72c78c6e18951e3d3a3771e7c44c443fc2bc8
Viloren88 <garifullin.marat@gmail.com>
2022-02-01 23:16:12 +0300

    4.3.1. Оператор && [#333783]

---
+++ src/main/java/ru/job4j/condition/Games.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Games {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        String result = allowByParent && hasMoney ? "I can go to the computer club." : "I can't.";
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
+        Games.permission(false, false);
+    }
+}
