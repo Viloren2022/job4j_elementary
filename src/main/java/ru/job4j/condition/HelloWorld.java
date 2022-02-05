d109d6c4e7b8fccfd505e8205531fa1359573da6
Viloren88 <garifullin.marat@gmail.com>
2022-02-05 16:20:43 +0300

    4.4. Вложенный оператор if else. Запутанный код [#190 #260109]

---
+++ src/main/java/ru/job4j/condition/HelloWorld.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class HelloWorld {
+    public static String checkNumber(int number) {
+        String rsl;
+        if (number % 3 == 0 && number % 5 == 0) {
+            rsl = "Hello, World";
+        } else if (number % 5 == 0) {
+            rsl = "World";
+        } else if (number % 3 == 0) {
+            rsl = "Hello";
+        } else {
+            rsl = "Operation not support";
+        }
+        return rsl;
+    }
+}
