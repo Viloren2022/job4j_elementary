ce9f4ea5bff78782857a3bf0ac93f4a4fa89423c
Viloren88 <garifullin.marat@gmail.com>
2022-02-11 21:08:17 +0300

    5.8. Именование классов и переменных [#4087 #261340]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int i = 0; i <= pref.length - 1; i++) {
-            if (word[i] != pref[i]) {
+        for (int k = 0; k <= pref.length - 1; k++) {
+            if (word[k] != pref[k]) {
                 result = false;
             }
         }
