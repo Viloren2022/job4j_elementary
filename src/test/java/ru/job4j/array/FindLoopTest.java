378e9aae389599ef5e5e1dada1a59c4912b6919f
Viloren88 <garifullin.marat@gmail.com>
2022-02-12 00:57:37 +0300

    6.5.2. Поиск минимума в диапазоне. [#179804]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -24,12 +24,12 @@ public class FindLoopTest {
 
     @Test
     public void whenFind3() {
-        int[] data = new int[] {5, 2, 10, 2, 4};
-        int el = 2;
-        int start = 2;
-        int finish = 4;
+        int[] data = new int[] {3, 2, 1};
+        int el = 1;
+        int start = 0;
+        int finish = 2;
         int result = FindLoop.indexOf(data, el, start, finish);
-        int expected = 3;
+        int expected = 2;
         Assert.assertEquals(expected, result);
     }
 }
