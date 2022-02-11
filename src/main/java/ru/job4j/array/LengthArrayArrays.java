cc3bac66397e5d6d65abbe0423655859d90e1b90
Viloren88 <garifullin.marat@gmail.com>
2022-02-12 01:39:15 +0300

    6.6.6.2. Двухмерный массив. Циклы.

--- src/main/java/ru/job4j/array/LengthArrayArrays.java
+++ src/main/java/ru/job4j/array/LengthArrayArrays.java
@@ -4,7 +4,12 @@ import java.sql.SQLOutput;
 
 public class LengthArrayArrays {
     public static void main(String[] args) {
-        int[][] numbers = {{1}, {1, 2}, {1, 2, 3}, {1, 2, 3, 4}};
+        int[][] numbers = {
+                {1},
+                {1, 2},
+                {1, 2, 3},
+                {1, 2, 3, 4}
+        };
         for (int[] number : numbers) {
             System.out.println("Размер вложенного массива равен: " + number.length);
         }
