8fad73367a68921188c00823825f6e81ed177232
Viloren88 <garifullin.marat@gmail.com>
2022-03-04 23:46:12 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/RightDiagonal.java
+++ src/main/java/ru/job4j/array/RightDiagonal.java
@@ -2,13 +2,13 @@ package ru.job4j.array;
 
 public class RightDiagonal {
     public static int[] diagonal(int[][] data) {
-        int[] array = new int[data.length];
+        int[] array2 = new int[data.length];
         int index = 0;
         for (int i = data.length - 1; i >= 0; i--) {
-            array[index] = data[index][i];
+            array2[index] = data[index][i];
             index++;
         }
-        return array;
+        return array2;
     }
 }
 
