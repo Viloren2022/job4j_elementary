adef94079f20d52f15b9cbf1b2e16fefb7fa7355
Viloren2022 <marat.garifullin88@gmail.com>
2022-03-07 22:16:47 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,13 +1,19 @@
 package ru.job4j.condition;
 
 public class Max {
+
     public static int max(int left, int right) {
         int result = left > right ? left : right;
         return result;
     }
 
-public static void main(String[] args) {
-    int result = Max.max(5, 8);
-    System.out.println(result);
+    public static int max(int first, int second, int third) {
+        int tmp = max(third, max(first, second));
+        return tmp;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int tmp = max(fourth, max(first, second, third));
+        return tmp;
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,26 @@ public class MaxTest {
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax5To2To3Then5() {
+        int first = 5;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 5;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax5To2To3To12Then12() {
+        int first = 5;
+        int second = 2;
+        int third = 3;
+        int fourth = 12;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 12;
+        Assert.assertEquals(result, expected);
+    }
+
 }
